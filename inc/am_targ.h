9246e7536663c4d160af4081d7b6edfae03a7392
Brandon Awbrey <nokojoe@hotmail.com>
2013-11-28 09:43:06 -0600

    Missing am_targ.h

---
+++ inc/am_targ.h
@@ -0,0 +1,11 @@
+#ifndef _AM_TARG_H
+#define _AM_TARG_H
+
+AM_RETURN am_targ_release(AM_MEM_FUNCTION_T *pFunc, void *p1, UINT32 *ret_len);
+AM_RETURN am_targ_open(AM_MEM_FUNCTION_T *pFunc, void *p1, UINT32 *ret_len);
+AM_RETURN am_targ_close(AM_MEM_FUNCTION_T *pFunc, void *p1, UINT32 *ret_len);
+
+
+
+
+#endif
